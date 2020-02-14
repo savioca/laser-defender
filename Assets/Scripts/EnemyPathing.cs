8d01c440ba34fc22c0b39ed92321e4f8c0cc7400
SAVIO ROSA OLIVEIRA <saviorosaoliveira@gmail.com>
2020-02-14 16:07:09 -0200

    implementei vida + loop wave

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {    
-    [SerializeField] WaveConfig waveConfig;
-    List<Transform> waypoints;
-    [SerializeField] float moveSpeed = 2f;
+    WaveConfig waveConfig;
+    List<Transform> waypoints;    
     int waypointIndex = 0;
 
     // Use this for initialization
@@ -21,13 +20,16 @@ public class EnemyPathing : MonoBehaviour
     {
         Move();
     }
-
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
     private void Move()
     {
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector3.MoveTowards
                  (transform.position, targetPosition, movementThisFrame);
 
